81c77803d39eb3310e713b1ba7d675dd6802c384
Molly Beach <mollyjbeach@gmail.com>
2026-09-14 22:13:50 -0700

    feat(props): Add musical instruments and hat props
    Add saxophone and noogler hat props. Add two new in-scene prop assets and register them in the Palais prop catalog and decor list so they can be used as decorative trinkets.

--- src/palais/shelves.ts
+++ src/palais/shelves.ts
@@ -133,6 +133,7 @@ export const SHELVES: Shelf[] = [
       "artwork_framed_print_woman_blue_leaves_sticker",
       "artwork_framed_print_red_poppies_pink_sticker",
       "artwork_framed_vogue_cover_art_deco_sticker",
+      "artwork_framed_abstract_splatter_painting_blue_green_gold_frame_sticker",
     ],
   },
   {
